aea2c37ac3a0e267c1f9d8cdd9d70010bfca83cb
trashcognito <denizmersinlioglu2005@gmail.com>
2021-11-25 17:14:06 +0300

    added more stuff

--- Daniel-D-sentrieb.ino
+++ Daniel-D-sentrieb.ino
@@ -1,23 +1,36 @@
 #include <Adafruit_TCS34725.h>
-
-#define FARBE_SENSOR_SDA 4   
-#define FARBE_SENSOR_SCL 5   
+ 
 #define MOTOR_LINKS_RUCKWARTS 3   
 #define MOTOR_RECHTS_RUCKWARTS 4   
-#define MOTOR_LINKS_PID 5   
-#define MOTOR_RECHTS_PID 6   
+#define MOTOR_LINKS_PWM 5   
+#define MOTOR_RECHTS_PWM 6   
 #define MOTOR_LINKS_VORWARTS 7   
 #define MOTOR_RECHTS_VORWARTS 8   
 #define LED_R 9   
 #define LED_G 10   
 #define LED_B 11
-
+/*
+Treibt den Motoren mit Richtung-Eingabe
+@param richtung Richtung zwischen -127 (ganz nach links) und 128 (ganz nach rechts) 
+*/
+void motor_richten(int richtung) {
+  richtung += 127;
+  analogWrite(MOTOR_LINKS_PWM, 255 - richtung);
+  
+}
 void setup() {
   // put your setup code here, to run once:
-  
+  pinMode(MOTOR_LINKS_RUCKWARTS, OUTPUT);
+  pinMode(MOTOR_RECHTS_RUCKWARTS, OUTPUT);
+  pinMode(MOTOR_LINKS_PWM, OUTPUT);
+  pinMode(MOTOR_RECHTS_PWM, OUTPUT);
+  pinMode(MOTOR_LINKS_VORWARTS, OUTPUT);
+  pinMode(MOTOR_RECHTS_VORWARTS, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
