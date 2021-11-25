e586adfc8791813d1f4dafb732e3d8902256cc4a
trashcognito <denizmersinlioglu2005@gmail.com>
2021-11-25 16:10:28 +0300

    added peripheral defines

--- Daniel-D-sentrieb.ino
+++ Daniel-D-sentrieb.ino
@@ -1,6 +1,20 @@
+#include <Adafruit_TCS34725.h>
+
+#define FARBE_SENSOR_SDA 4   
+#define FARBE_SENSOR_SCL 5   
+#define MOTOR_LINKS_RUCKWARTS 3   
+#define MOTOR_RECHTS_RUCKWARTS 4   
+#define MOTOR_LINKS_PID 5   
+#define MOTOR_RECHTS_PID 6   
+#define MOTOR_LINKS_VORWARTS 7   
+#define MOTOR_RECHTS_VORWARTS 8   
+#define LED_R 9   
+#define LED_G 10   
+#define LED_B 11
+
 void setup() {
   // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
