99fb6c5c84a3779103f5ee61b3f0b5d17b8151b3
trashcognito <denizmersinlioglu2005@gmail.com>
2021-11-26 16:55:16 +0300

    liniefolge repariert

--- Daniel-D-sentrieb.ino
+++ Daniel-D-sentrieb.ino
@@ -12,19 +12,20 @@
 #define LED_B 11
 
 //TODO: Diese Werte im echten Leben probieren!
-#define PLATTCHEN_FARBE_FEHLER 30 /* Deviation von den erwarteten Farben, die Plättchen besitzen */
+#define PLATTCHEN_FARBE_FEHLER 110 /* Deviation von den erwarteten Farben, die Plättchen besitzen */
 #define INFRAROT_WEISS_SCHWELLE 100
 #define KORRIGIERUNGSRICHTUNG_RECHTS 30
 #define KORRIGIERUNGSRICHTUNG_LINKS 50
 /* Automatisch berechnet, nicht direkt geändern! */
 
-#define FARBE_OBERE_SCHWELLE (255-PLATTCHEN_FARBE_FEHLER)
-#define FARBE_UNTERE_SCHWELLE (PLATTCHEN_FARBE_FEHLER)
+#define FARBE_SPEZIFISCH_STELLE 110
+#define FARBE_WEISS_SCHWELLE 60
+#define WEISS_LUX_SCHWELLE 500
 
 
+void farbesensor_lesen(float *r, float *g, float *b, uint16_t *lux=NULL) {
+	uint16_t r_raw, g_raw, b_raw, clear_raw, lux_raw;
 
-void farbesensor_lesen(float *r, float *g, float *b) {
-	uint16_t r_raw, g_raw, b_raw, clear_raw;
 	i2c_start_wait(0xb4);
 	i2c_write(0x00);
 	i2c_start_wait(0xb5);
@@ -35,12 +36,29 @@ void farbesensor_lesen(float *r, float *g, float *b) {
 	b_raw = i2c_readAck() << 8;
 	b_raw |= i2c_readAck();
 	clear_raw = i2c_readAck() << 8;
-	clear_raw |= i2c_readNak();
+	if (!lux) {
+		clear_raw |= i2c_readNak();
+	} else {
+		clear_raw |= i2c_readAck();
+		lux_raw = i2c_readAck() << 8;
+		lux_raw |= i2c_readNak();
+	}
+	
     i2c_stop();
 
 	*r = ((float)r_raw / (float)clear_raw) * 255;
 	*g = ((float)g_raw / (float)clear_raw) * 255;
 	*b = ((float)b_raw / (float)clear_raw) * 255;
+	if (lux) {
+		*lux = lux_raw;
+	}
+}
+
+bool farbensensor_weiss() {
+	float r, g, b;
+	uint16_t lux;
+	farbesensor_lesen(&r, &g, &b, &lux);
+	return (r > FARBE_WEISS_SCHWELLE && g > FARBE_WEISS_SCHWELLE && b > FARBE_WEISS_SCHWELLE && lux > WEISS_LUX_SCHWELLE);
 }
 
 /*
@@ -58,19 +76,19 @@ void plattchen_behandeln() {
 	float r, g, b; /* Rot, Grün, Blau */
 	farbesensor_lesen(&r, &g, &b);
 	
-	if (r < FARBE_UNTERE_SCHWELLE && g > FARBE_OBERE_SCHWELLE && b < FARBE_UNTERE_SCHWELLE) {
+	if (g > FARBE_SPEZIFISCH_STELLE) {
 		/* Grünes Plättchen, LED soll grün sein */
-		digitalWrite(LED_R, LOW);
-		digitalWrite(LED_G, HIGH);
-		
-	} else if (r > FARBE_OBERE_SCHWELLE && g < FARBE_UNTERE_SCHWELLE && b < FARBE_UNTERE_SCHWELLE) {
-		/* Rotes Plättchen, LED soll rot sein */
 		digitalWrite(LED_R, HIGH);
 		digitalWrite(LED_G, LOW);
+		
+	} else if (r > FARBE_SPEZIFISCH_STELLE) {
+		/* Rotes Plättchen, LED soll rot sein */
+		digitalWrite(LED_R, LOW);
+		digitalWrite(LED_G, HIGH);
 	} else {
 		/* Kein Plättchen, LED ausgeschaltet */
-		digitalWrite(LED_R, LOW);
-		digitalWrite(LED_G, LOW);
+		digitalWrite(LED_R, HIGH);
+		digitalWrite(LED_G, HIGH);
 	}
 
 }
@@ -78,13 +96,7 @@ void plattchen_behandeln() {
 void linie_folgen() {
 	float r, g, b;
 	farbesensor_lesen(&r, &g, &b);
-	if (r < FARBE_OBERE_SCHWELLE || g < FARBE_OBERE_SCHWELLE || b < FARBE_OBERE_SCHWELLE) {
-		//Farbensensor sieht nicht weiß
-
-		//Wir sind auf dem Linie, nichts zu tun als vorwärts zu gehen. (?)
-		//TODO: Probiert das echt!
-		motoren_richten(0);
-	} else {
+	if (farbensensor_weiss()) {
 		//Farbensensor sieht weiß
 		//Infrarot-Sensor muss probiert werden
 		//TODO: Resettieren des Infrarots benötigt?
@@ -101,6 +113,12 @@ void linie_folgen() {
 			//Das Roboter soll sich nach links bewegen
 			motoren_richten(KORRIGIERUNGSRICHTUNG_LINKS);
 		}
+	} else {
+		//Farbensensor sieht nicht weiß
+
+		//Wir sind auf dem Linie, nichts zu tun als vorwärts zu gehen. (?)
+		//TODO: Probiert das echt!
+		motoren_richten(0);
 	}
 }
 
@@ -117,9 +135,9 @@ void setup() {
 	pinMode(LED_B, OUTPUT);
 	pinMode(INFRAROT_SENSOR, INPUT);
 
-	digitalWrite(LED_R, LOW);
-	digitalWrite(LED_G, LOW);
-	digitalWrite(LED_B, LOW);
+	digitalWrite(LED_R, HIGH);
+	digitalWrite(LED_G, HIGH);
+	digitalWrite(LED_B, HIGH);
 	
 	//Motoren orientieren, bei SKS1 nur nach vorne
 	digitalWrite(MOTOR_LINKS_RUCKWARTS, 0);
